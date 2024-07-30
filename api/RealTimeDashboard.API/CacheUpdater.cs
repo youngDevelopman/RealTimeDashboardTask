784bbb42062373bfe65031f853cb9a26dee39207
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-07-30 21:50:23 +0300

    Add cache as a main source of truth

---
+++ api/RealTimeDashboard.API/CacheUpdater.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.Extensions.Caching.Memory;
+
+namespace RealTimeDashboard.API
+{
+    public class CacheUpdater : BackgroundService
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public CacheUpdater(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+            _memoryCache.Set("active-users", 50);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(-10, 11);
+                var currentActiveUsers = _memoryCache.Get<int>("active-users");
+                currentActiveUsers += randomNumber;
+                _memoryCache.Set("active-users", currentActiveUsers);
+
+                await Task.Delay(2000, stoppingToken);
+            }
+        }
+    }
+}
--- api/RealTimeDashboard.API/Program.cs
+++ api/RealTimeDashboard.API/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Hosting;
+using RealTimeDashboard.API;
 using System.Net;
 using System.Net.WebSockets;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:6969");
+builder.Services.AddMemoryCache();
+builder.Services.AddHostedService<CacheUpdater>();
 var app = builder.Build();
+
 app.UseWebSockets();
 
 app.Map("/ws/active-users", async context =>
@@ -12,13 +18,26 @@ app.Map("/ws/active-users", async context =>
     if (context.WebSockets.IsWebSocketRequest)
     {
         using var ws = await context.WebSockets.AcceptWebSocketAsync();
-        var message = "Hello from Nazar!";
-        var bytes = Encoding.UTF8.GetBytes(message);
-        var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
-        await ws.SendAsync(arraySegment,
-                            WebSocketMessageType.Text,
-                            true,
-                            CancellationToken.None);
+        var cache = context.RequestServices.GetRequiredService<IMemoryCache>();
+        while (true)
+        {
+            if(ws.State == WebSocketState.Open)
+            {
+                var activeUsers = cache.Get<int>("active-users");
+                string message = $"Current amount of active users is {activeUsers}";
+                var bytes = Encoding.UTF8.GetBytes(message);
+                var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+                await ws.SendAsync(arraySegment,
+                                    WebSocketMessageType.Text,
+                                    true,
+                                    CancellationToken.None);
+            }
+            else if(ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
+            {
+                break;
+            }
+            await Task.Delay(5000);
+        }
     }
     else
     {
